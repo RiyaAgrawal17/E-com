53031014c3c8095b1ea65172581850aaf17aa4a8
RiyaAgrawal17 <65159160+RiyaAgrawal17@users.noreply.github.com>
2021-01-06 17:55:15 +0530

    initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import { Card, Button, CardDeck } from "react-bootstrap";
+import { Card, Button, Row, Col } from "react-bootstrap";
 import {CardData} from './Data';
 
 
@@ -9,12 +9,8 @@ function Home() {
       <div style={{marginTop : "100px"}}>
       <Jumbotron className="jumbo">
   <h1>Hello, world!</h1>
-  <p style={{fontSize : "20px"}}>
+  <p>
     This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.This is a simple hero unit, a simple jumbotron-style component for calling
     extra attention to featured content or information.This is a simple hero unit, a simple jumbotron-style component for calling
     extra attention to featured content or information.This is a simple hero unit, a simple jumbotron-style component for calling
     extra attention to featured content or information.This is a simple hero unit, a simple jumbotron-style component for calling
@@ -24,11 +20,11 @@ function Home() {
 </Jumbotron>
 
 
-
+<Row>
 {CardData.map((data) => {
   const {title,price} = data;
   return (
-   <CardDeck> 
+    <Col lg={4} md={6} sm={12}>
 <Card style={{width : "18rem"}} className="card" >
   <Card.Img className="img" src="" />
   <Card.Body>
@@ -39,10 +35,10 @@ function Home() {
     <Button className="btn">Add to cart</Button>
   </Card.Body>
 </Card>
-</CardDeck>
-        );
-      })}
-      
+</Col>
+ );
+ })}
+</Row> 
     </div>
 );
 }
