7cef17b0ed1ae8c171aa80db7f6b00d8fa42f022
Riya Agrawal <65159160+RiyaAgrawal17@users.noreply.github.com>
2021-01-08 22:29:19 +0530

    cart ui added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ListGroup,Row, Col } from "react-bootstrap";
 
 
-function Cart() {
+export default function Cart() {
+
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const product = JSON.parse(localStorage.getItem("cart"));
+    if (product) {
+      setCart(product);
+    }
+  }, []);
+
+  const handleClick = (e) => {
+    console.log("I am clicked");
+  }
+
   return (
-    <div style={{marginTop : "50px"}}>
-       Cart page
+    <div style={{marginTop : "70px"}}>
+       <h2>Cart</h2>
+
+        <ListGroup>
+        {cart.map((item) => {
+          return (
+            <ListGroup className="my-3">
+              <ListGroup.Item>
+                <Row>
+                  <Col className="my-auto" sm={4}>
+                    {item.name}
+                  </Col>
+                  <Col className="my-auto" sm={4}>
+                    <i
+                      onClick={handleClick}
+                      className="lni lni-circle-plus mr-2"
+                    ></i>
+                    {item.stock}
+                    <i
+                      onClick={handleClick}
+                      className="lni lni-circle-minus ml-2"></i>
+                  </Col>
+                  <Col className="my-auto" sm={2}>
+                     {item.price}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          );
+          })}
+      </ListGroup>
       </div>
-   
   );
 }
 
-export default Cart;
