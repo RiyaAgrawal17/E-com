125b99a2477d39940dd9d959866ac7a9e3c36570
Riya Agrawal <65159160+RiyaAgrawal17@users.noreply.github.com>
2021-01-11 13:20:12 +0530

    Cart functionality & add to cart

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,50 +1,89 @@
-import React from "react";
-import {Jumbotron} from 'react-bootstrap';
+import React, {useState, useEffect} from "react";
 import { Card, Button, Row, Col } from "react-bootstrap";
-import {CardData} from './Data';
 
-function Home() {
 
-  function btnClick() {
-    console.log("I am clicked!");
-  }
+const Home = () => {
 
+  const [locals, setLocals] = useState([
+    {
+      id: "",
+      name: "",
+      price: "",
+      description: "",
+    },
+  ]);
+
+  const search = (id, arr) => {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i].id === id) {
+        return arr[i];
+      }
+    }
+  };
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("cart"));
+
+    if (!data) {
+      localStorage.setItem("cart", "[]");
+    }
+
+  }, []);
+
+  
+  useEffect(() => {
+      const products = JSON.parse(localStorage.getItem("product"));
+      if (!products) {
+        localStorage.setItem("product", "[]");
+      }
+      
+
+    const local = JSON.parse(
+      localStorage.getItem("product")
+    );
+
+    if (local) {
+      setLocals(local);
+    }
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const products = JSON.parse(localStorage.getItem("product"));
+    const getCartItem = search(e.target.id, products);
+
+    const cartItem = JSON.parse(localStorage.getItem("cart"));
+    cartItem.push(getCartItem);
+    localStorage.setItem("cart", JSON.stringify(cartItem));
+  };
+
+ 
+
+
+  const product = locals.map((item) => {
+    console.log(item);
+    return (
+      <div style={{marginTop:"70px"}}>
+      <Col lg={12} md={6} sm={12} key={item.id}>
+        <Card className="my-3">
+          <Card.Img variant="top" />
+          <Card.Body>
+
+            <h4>{item.name}</h4>
+            <p>₹{item.price}</p>
+            <p>{item.description}</p>
+            <Button block id={item.id} onClick={handleSubmit} variant="primary">
+              Add to Cart
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+     </div>
+    );
+  });
   return (
-      <div style={{marginTop : "100px"}}>
-      <Jumbotron className="jumbo">
-  <h1>Hello, world!</h1>
-  <p>
-    This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.
-  </p>
-</Jumbotron>
-
-
-<Row>
-{CardData.map((data) => {
-  const {title,price} = data;
-  return (
-    <Col lg={4} md={6} sm={12}>
-<Card style={{width : "18rem"}} className="card" >
-  <Card.Img className="img" src="" />
-  <Card.Body>
-    <Card.Title>{title}</Card.Title>
-    <Card.Text>
-      Price : {price}
-    </Card.Text>
-    <Button className="btn" onClick={btnClick}>Add to cart</Button>
-  </Card.Body>
-</Card>
-</Col>
- );
- })}
-</Row> 
-</div>
-);
-}
+      <Row>{product}</Row>
+  );
+};
 
 export default Home;
